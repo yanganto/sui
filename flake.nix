b3f8ab5e2ce50ff691e4ada326b9d751c548b303
Antonio Yang <yanganto@gmail.com>
2024-10-21 15:04:05 +0800

    ci: add flake and shells

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    crane.inputs.flake-utils.follows = "flake-utils";
+    crane.inputs.rust-overlay.follows = "rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, crane, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        packages = (import ./nix/packages.nix { inherit self pkgs crane; toolchain = ./rust-toolchain.toml; });
+      in
+      {
+        inherit packages;
+        devShells = (import ./nix/shell.nix { inherit pkgs packages; toolchain = ./rust-toolchain.toml; });
+      }
+    );
+}
