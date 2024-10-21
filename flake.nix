02fd8abf96d2e89575fdc4ab4199a2e2e8ce0223
Antonio Yang <yanganto@gmail.com>
2024-10-21 15:04:05 +0800

    ci: add flake and dev shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells =  (import ./nix/shell.nix { inherit pkgs; toolchain = ./rust-toolchain.toml; });
+      }
+    );
+}
---
+++ nix/shell.nix
@@ -0,0 +1,34 @@
+{ pkgs, toolchain }:
+let
+  specificRust = pkgs.rust-bin.fromRustupToolchainFile toolchain;
+in
+{
+  # Dev shell for following binaries
+  # anemo-benchmark, cut, import-trace, move-analyzer, sui-framework-snapshot,
+  # sui-light-client, sui-metric-checker, sui-move, sui-oracle, sui-proxy,
+  # sui-source-validation-service, sui-test-validator, suiop
+  core = pkgs.mkShell ({
+    name = "core";
+    buildInputs = with pkgs; [specificRust ];
+    DEV_SHELL_NAME = "core";
+  });
+
+  # Dev shell for most binaies
+  # NOTE: sui-aws-orchestrator needs addtional dependency aws-sdk-ec2
+  default = pkgs.mkShell ({
+    buildInputs = with pkgs; [
+      specificRust
+      openssl
+    ];
+    nativeBuildInputs = with pkgs; [
+      clang
+      libclang.lib
+      llvmPackages.libcxxClang
+      pkg-config
+    ];
+    DEV_SHELL_NAME = "sui";
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libcxxClang}/resource-root/lib/";
+
+  });
+}
