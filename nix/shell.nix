b0f7f1d5845a129ff9f87f1df031cb8b57a55e0c
Antonio Yang <yanganto@gmail.com>
2024-10-21 15:04:05 +0800

    ci: add flake and shells

---
+++ nix/shell.nix
@@ -0,0 +1,43 @@
+{ pkgs, toolchain, packages }:
+let
+  specificRust = pkgs.rust-bin.fromRustupToolchainFile toolchain;
+in
+{
+  # Dev shell for following binaries
+  # anemo-benchmark, cut, import-trace, move-analyzer, sui-framework-snapshot,
+  # sui-light-client, sui-metric-checker, sui-move, sui-oracle, sui-proxy,
+  # sui-source-validation-service, sui-test-validator, suiop
+  core = pkgs.mkShell ({
+    name = "core";
+    buildInputs = with pkgs; [specificRust ];
+    DEV_SHELL_NAME = "sui#core";
+  });
+
+  # Dev shell for most binaies
+  # NOTE: sui-aws-orchestrator needs addtional dependency aws-sdk-ec2
+  dev = pkgs.mkShell ({
+    buildInputs = with pkgs; [
+      specificRust
+      openssl
+    ];
+    nativeBuildInputs = with pkgs; [
+      clang
+      libclang.lib
+      llvmPackages.libcxxClang
+      pkg-config
+    ];
+    DEV_SHELL_NAME = "sui#dev";
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libcxxClang}/resource-root/lib/";
+  });
+
+  default = pkgs.mkShell ({
+    buildInputs = with packages; [ sui ];
+    DEV_SHELL_NAME = "sui#default";
+  });
+
+  slim = pkgs.mkShell ({
+    buildInputs = with packages; [ sui-move ];
+    DEV_SHELL_NAME = "sui#slim";
+  });
+}
